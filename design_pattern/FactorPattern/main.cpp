885b7f7f38bf6cc715a25d62e93e158d2290cb52
duyongUbuntu <tyduyong@126.com>
2018-07-25 03:46:50 +0800

    design_pattern

---
+++ design_pattern/FactorPattern/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << 11 << endl;
+	return 0;
+}
---
+++ design_pattern/FactorPattern/operatorBase.h
@@ -0,0 +1,10 @@
+/*
+ *操作符类的基类
+ * */
+#ifdef __OPERATORBASE__H_
+#define __OPERATORBASE__H_
+class operatorBase{
+	
+
+};
+#endif //__OPERATORBASE__H_
